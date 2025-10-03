7d403efb82ed3361b5fff08d9655fd3439dcff03
Antonio Herrera Martin <44150484+alfa33333@users.noreply.github.com>
2025-10-03 14:45:23 -0600

    complete exercise 3

--- src/components/ShoeIndex/ShoeIndex.jsx
+++ src/components/ShoeIndex/ShoeIndex.jsx
@@ -25,7 +25,7 @@ const ShoeIndex = ({ sortId, setSortId }) => {
           </Select>
         </Header>
         <Spacer size={34} />
-        <ShoeGrid />
+        {/* <ShoeGrid /> */}
       </MainColumn>
       <LeftColumn>
         <Breadcrumbs>
@@ -42,15 +42,26 @@ const ShoeIndex = ({ sortId, setSortId }) => {
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: flex-end;
+`;
 
-const LeftColumn = styled.div``;
+const LeftColumn = styled.div`
+  flex: 0 0  248px;
+`;
 
-const MainColumn = styled.div``;
+const MainColumn = styled.div`
+  flex: 1;
+`;
 
-const Header = styled.header``;
+const Header = styled.header`
+  display: flex;
+`;
 
 const Title = styled.h2`
+flex: 1;
   font-size: 1.5rem;
   font-weight: ${WEIGHTS.medium};
 `;
